1ad4515b6ae08ac480e761771a8275969269605e
Arkam <arkam.ardsb@gmail.com>
2022-07-11 15:52:07 +0530

    crud function has been developed

--- src/main/java/net/javaguides/springboot/controller/UserController.java
+++ src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -1,8 +1,8 @@
 package net.javaguides.springboot.controller;
 
 import net.javaguides.springboot.exception.ResourceNotFoundException;
+import net.javaguides.springboot.model.User;
 import net.javaguides.springboot.repository.UserRepository;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,43 +24,45 @@ public class UserController {
     }
 
     @PostMapping
-    public net.javaguides.springboot.model.User createEmployee(@RequestBody net.javaguides.springboot.model.User user) {
+    public net.javaguides.springboot.model.User createUser(@RequestBody net.javaguides.springboot.model.User user) {
         return userRepository.save(user);
     }
 
-//    // build get employee by id REST API
-//    @GetMapping("{id}")
-//    public ResponseEntity<User> getEmployeeById(@PathVariable  long id){
-//        User employee = employeeRepository.findById(id)
-//                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
-//        return ResponseEntity.ok(employee);
-//    }
+    // build get employee by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<net.javaguides.springboot.model.User> getUserById(@PathVariable  long id){
+        net.javaguides.springboot.model.User user =  userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id:" + id));
+        return ResponseEntity.ok(user);
+    }
 
-    // build update employee REST API
-//    @PutMapping("{id}")
-//    public ResponseEntity<User> updateEmployee(@PathVariable long id,@RequestBody User employeeDetails) {
-//        User updateEmployee = employeeRepository.findById(id)
-//                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
-//
-//        updateEmployee.setFirstName(employeeDetails.getFirstName());
-//        updateEmployee.setLastName(employeeDetails.getLastName());
-//        updateEmployee.setEmailId(employeeDetails.getEmailId());
-//
-//        employeeRepository.save(updateEmployee);
-//
-//        return ResponseEntity.ok(updateEmployee);
-//    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@PathVariable long id, @RequestBody net.javaguides.springboot.model.User userDetails) {
+         User updateUser = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+
+        updateUser.setFirstName(userDetails.getFirstName());
+        updateUser.setLastName(userDetails.getLastName());
+        updateUser.setEmailId(userDetails.getEmailId());
+        updateUser.setPassword(userDetails.getPassword());
+        updateUser.setUserRole(userDetails.getUserRole());
+
+        userRepository.save(updateUser);
+
+        return ResponseEntity.ok(updateUser);
+    }
 
     // build delete employee REST API
-//    @DeleteMapping("{id}")
-//    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id){
-//
-//        User employee = employeeRepository.findById(id)
-//                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
-//
-//        employeeRepository.delete(employee);
-//
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//
-//    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable long id){
+
+        User User = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id: " + id));
+
+        userRepository.delete(User);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
 }
